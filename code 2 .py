75a844cd0c77d39d1a0a06aee60db10dd89e9d4f
Sree <132946834+Sreejith-nair511@users.noreply.github.com>
2023-12-17 17:14:39 +0530

    Add files via upload

---
+++ code 2 .py
@@ -0,0 +1,8 @@
+#to find if the entry is senior citizen or not 
+Name=input('enter the name')
+Y1=int(input('enter  birth year'))
+age=2023-Y1
+if age>55:
+    print(Name,'is senior citizen')
+else:
+    print(Name,'is not a senior citizen')
