f0f81136b5f9276c99a9e622576988c1e82f8916
Sree <132946834+Sreejith-nair511@users.noreply.github.com>
2023-12-19 15:52:21 +0530

    Add files via upload

---
+++ code7.py
@@ -0,0 +1,15 @@
+#factorial kurma curry
+
+
+def factorial(num):
+    if num==0:
+        return 1
+    else:
+        return num*factorial(num-1)
+    
+n=int(input('enter the value of n:'))
+r=int(input('enter the r value(r cannot be negative or greater than n)'))
+nCr=factorial(n)/factorial(r)*factorial(n-r)
+print(n,'C',r," = ","%d"%nCr,sep="")
+
+    
